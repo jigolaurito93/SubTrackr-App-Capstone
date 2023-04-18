a7ebc91cb155fbc7f5525de810942cc46db561af
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-17 22:10:27 -0500

    Add logo for index.html and description

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route("/")
 def hello():
-    return render_template("base.html")
+    return render_template("index.html")
