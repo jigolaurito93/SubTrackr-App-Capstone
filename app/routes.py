e5c5a4b54ebc7f0805f9a7d34b773d38ca2436d3
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-17 22:15:59 -0500

    add login and signup routes, add login and signup.html

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,11 @@ from flask import render_template, url_for
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
