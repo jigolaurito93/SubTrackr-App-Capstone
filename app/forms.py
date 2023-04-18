d3d5016b41f5c902c7d2bfc98fd0961d1a252390
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-18 00:07:56 -0500

    redirect to login page after sign up

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import EqualTo, InputRequired
+
+class SignUpForm(FlaskForm):
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.forms import SignUpForm
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignUpForm()
-
+    if form.validate_on_submit():
+        print('Horaaaay')
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(first_name, last_name, email, username, password)
+        return redirect(url_for("login"))
     return render_template("signup.html", form=form)
 
 @app.route("/login")
