941387cebe1f023b0aefbea5a0ad64fa6963690b
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-18 00:26:56 -0500

    import flash, added alerts for flash

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm
 
 @app.route("/")
@@ -17,7 +17,8 @@ def signup():
         username = form.username.data
         password = form.password.data
         print(first_name, last_name, email, username, password)
-        return redirect(url_for("login"))
+        flash(f"Thank you {first_name} for signing up!", "success")
+        return redirect(url_for('login'))
     return render_template("signup.html", form=form)
 
 @app.route("/login")
